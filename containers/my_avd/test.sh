6f9d9fd99c464442f402fefef624f5c8f289dd1a
ankudinov <petr.ankudinov@gmail.com>
2024-03-06 17:05:22 +0100

    try with env var

--- containers/my_avd/test.sh
+++ containers/my_avd/test.sh
@@ -2,4 +2,4 @@
 set -e
 
 # test if cLab version is correct
-if [ -z "$(containerlab version | grep 'version: 0.51.3')" ]; then exit 1; fi
+if [ -z "$(containerlab version | grep \"version: ${CLAB_VERSION}\")" ]; then exit 1; fi
