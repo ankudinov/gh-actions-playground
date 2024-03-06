80e774233b13858c34cfa5f7752a9ca0f17d3587
ankudinov <petr.ankudinov@gmail.com>
2024-03-06 17:19:22 +0100

    try updated test script

--- containers/my_avd/test.sh
+++ containers/my_avd/test.sh
@@ -2,4 +2,11 @@
 set -e
 
 # test if cLab version is correct
-if [ -z "$(containerlab version | grep \"version: ${CLAB_VERSION}\")" ]; then exit 1; fi
+echo ">>> Testing cLab version... Must be ${CLAB_VERSION}"
+TEST_OUT=$(containerlab version | grep "version: ${CLAB_VERSION}")
+if [ -z "${TEST_OUT}" ]; then
+    echo "      cLab version is incorrect."
+    exit 1
+else
+    echo "      cLab version is correct."
+fi
